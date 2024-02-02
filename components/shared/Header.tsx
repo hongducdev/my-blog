91672f86e7e1b335670262d66e4d7f2cdf442627
hongducdev <contact.hongduc@gmail.com>
2024-02-02 22:53:36 +0700

    Theme toggle and redo Logo

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
     <header className="w-full fixed top-0 backdrop-blur-sm border-b border-neutral-300">
       <div className="max-w-[1240px] w-full mx-auto py-3 flex items-center justify-between">
         <Logo />
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-5">
           <Link
             href="https://hongduccodedao.io.vn"
-            className="text-neutral-500 font-medium hover:text-green-1000 transition-all duration-300 ease-in-out"
+            className="text-neutral-500 dark:text-neutral-300 font-medium hover:text-green-1000 transition-all duration-300 ease-in-out"
           >
             My Portfolio
           </Link>
--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="bg-gradient-to-r from-[#a6e3a1] to-[#94e2d5] inline-block px-7 py-1 text-white font-semibold text-lg">
-      hongducdev
+    <div className="flex items-center gap-3">
+      <div className="bg-gradient-to-r w-10 h-10 from-[#a6e3a1] to-[#94e2d5] p-2 text-white font-bold text-lg flex items-center justify-center rounded-sm">
+        {"</>"}
+      </div>
+      <div>
+        <span className="text-xl font-bold leading-none">hongducdev</span>
+        <p className="text-xs">Programming for life!</p>
+      </div>
     </div>
   );
 };
